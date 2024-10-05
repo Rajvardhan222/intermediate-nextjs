d619b0079c2eba68020b926ddc7a5ec15364eed6
Rajvardhan Singh Ranawat <146507318+Rajvardhan222@users.noreply.github.com>
2024-10-05 06:09:22 +0000

    Refactor middleware and constants file

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,11 +1,4 @@
-import { NextRequest } from "next/server"
+
 
 export const COOKIE_NAME = 'pardy-token'
 
-export function middleware(req: NextRequest) {
-    
-}
-
-export const config = {
-    matcher : ['/dashboard/:path*','/']
-}
