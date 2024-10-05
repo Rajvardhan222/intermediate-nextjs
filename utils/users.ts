96ac8f4a367e14d76e9f65be34cfd6163b521004
Rajvardhan Singh Ranawat <146507318+Rajvardhan222@users.noreply.github.com>
2024-10-05 02:03:40 +0000

    Refactor dashboard page to display attendee count and add RSVP functionality

--- utils/users.ts
+++ utils/users.ts
@@ -3,14 +3,15 @@ import { COOKIE_NAME } from './constants'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { getUserFromToken } from './authTools'
+import { cache } from 'react'
 
-export const getCurrentUser = async () => {
+export const getCurrentUser = cache( async () => {
     const token = cookies().get(COOKIE_NAME)
     if (!token) return redirect('/signin')
 
-    const user = await getUserFromToken(token)
+    const user = await getUserFromToken(token) 
 
     if (!user) return redirect('/signin')
 
     return user
-}
+})
